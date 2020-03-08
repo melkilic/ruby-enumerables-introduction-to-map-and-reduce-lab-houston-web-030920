5fcdb568df38bb250cc237ea86a6bac959fe714e
Melike KILIC <melike180400@gmail.com>
2020-03-08 22:48:49 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,5 +3,8 @@
    new_arr = []
    i = 0
    while i < source_array.length do
-     new_arr.push(source_array[i] * -1 )
+     new_arr.push(source_array[i] * -1)
+     i += 1
+   end
+   return new_arr
 end
