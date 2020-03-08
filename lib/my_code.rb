a10d3eef614a68c8af7b06456c875837cbe32a65
Melike KILIC <melike180400@gmail.com>
2020-03-08 23:08:02 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -60,7 +60,7 @@ end
 def reduce_to_any_true(source_array)
   i = 0
   while i < source_array.length do
-    return true if !source_array[i]
+    return false if !source_array[i]
     i += 1
   end
 end
