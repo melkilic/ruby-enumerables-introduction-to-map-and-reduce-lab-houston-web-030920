e8c7fc217de65c9d6b451edf87a0de96dd7d532f
Melike KILIC <melike180400@gmail.com>
2020-03-08 23:00:18 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -43,7 +43,7 @@ def reduce_to_total(source_array, starting_point = 0)
   new_arr = starting_point
   i = 0
   while i < source_array.length do
-    new += source_array[i]
+    new_arr += source_array[i]
     i += 1
   end
   return new
