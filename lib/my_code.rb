ee7b801e0e87cdfd6b6debf90e6b9615e5b43df2
Melike KILIC <melike180400@gmail.com>
2020-03-08 22:57:34 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -33,7 +33,7 @@ def map_to_square(source_array)
   new_arr = []
   i = 0
   while i < source_array.length do
-    new_arr.push(source_array[i] ** source_array[i])
+    new_arr.push(source_array[i] * source_array[i])
     i += 1
   end
   return new_arr
