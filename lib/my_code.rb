c94a2fa8791dfba471d6c97da855915ed1b275e6
Melike KILIC <melike180400@gmail.com>
2020-03-08 23:12:03 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,68 @@
 # My Code here....
+ def map_to_negativize(source_array)
+   new_arr = []
+   i = 0
+   while i < source_array.length do
+     new_arr.push(source_array[i] * -1)
+     i += 1
+   end
+   return new_arr
+end
+
+def map_to_no_change(source_array)
+  new_arr = []
+  i = 0
+  while i< source_array.length do
+    new_arr.push(source_array[i])
+    i += 1
+  end
+  return new_arr
+end
+
+def map_to_double(source_array)
+  new_arr = []
+  i = 0
+  while i < source_array.length do
+    new_arr.push(source_array[i] * 2)
+    i += 1
+  end
+  return new_arr
+end
+
+def map_to_square(source_array)
+  new_arr = []
+  i = 0
+  while i < source_array.length do
+    new_arr.push(source_array[i] * source_array[i])
+    i += 1
+  end
+  return new_arr
+end
+
+def reduce_to_total(source_array, starting_point = 0)
+  value = starting_point
+  i = 0
+  while i < source_array.length do
+    value += source_array[i]
+    i += 1
+  end
+  return value
+end
+
+def reduce_to_all_true(source_array)
+  i = 0
+  while i < source_array.length do
+   return false if !source_array[i]
+   i += 1
+  end
+  return true
+end
+
+def reduce_to_any_true(source_array)
+  i = 0
+  while i < source_array.length do
+    return true if source_array[i]
+    i += 1
+  end
+  return false
+end
