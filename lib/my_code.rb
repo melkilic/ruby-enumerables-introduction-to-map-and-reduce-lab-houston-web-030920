65d66f1ba086925ed2e108d1ab921269f7c8f8de
Melike KILIC <melike180400@gmail.com>
2020-03-08 23:00:13 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -39,6 +39,12 @@ def map_to_square(source_array)
   return new_arr
 end
 
-def reduce_to_total(source_array, starting_point)
-  
+def reduce_to_total(source_array, starting_point = 0)
+  new_arr = starting_point
+  i = 0
+  while i < source_array.length do
+    new += source_array[i]
+    i += 1
+  end
+  return new
 end
