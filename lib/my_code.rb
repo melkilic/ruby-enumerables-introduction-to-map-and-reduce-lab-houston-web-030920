de7b1654f35a1d0d6ea1c9e2f55c6d9277550847
Melike KILIC <melike180400@gmail.com>
2020-03-08 22:57:10 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -28,3 +28,13 @@ def map_to_double(source_array)
   end
   return new_arr
 end
+
+def map_to_square(source_array)
+  new_arr = []
+  i = 0
+  while i < source_array.length do
+    new_arr.push(source_array[i] ** source_array[i])
+    i += 1
+  end
+  return new_arr
+end
